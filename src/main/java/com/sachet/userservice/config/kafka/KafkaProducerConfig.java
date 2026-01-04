106b9d4d9f59d0307efb3a64361594cd9075a958
sachetwasti <sachetwasti61@gmail.com>
2026-01-04 22:54:21 +0530

    changed kafka-configuration

--- src/main/java/com/sachet/userservice/config/kafka/KafkaProducerConfig.java
+++ src/main/java/com/sachet/userservice/config/kafka/KafkaProducerConfig.java
@@ -17,23 +17,23 @@ import java.util.Map;
 @Configuration
 public class KafkaProducerConfig {
 
-    public Map<String, Object> producerConfig(EnvironmentConfiguration environmentConfiguration) {
-        Map<String, Object> props = new HashMap<>();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, environmentConfiguration.getKafkaConfiguration().getBootstrapServers());
-        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        return props;
-    }
-
-    @Bean
-    public ProducerFactory<String, String> producerFactory(@Autowired EnvironmentConfiguration environmentConfiguration) {
-        return new DefaultKafkaProducerFactory<>(producerConfig(environmentConfiguration));
-    }
-
-
-    @Bean
-    public KafkaTemplate<String, String> kafkaTemplate(ProducerFactory<String, String> producerFactory) {
-        return new KafkaTemplate<>(producerFactory);
-    }
+//    public Map<String, Object> producerConfig(EnvironmentConfiguration environmentConfiguration) {
+//        Map<String, Object> props = new HashMap<>();
+//        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, environmentConfiguration.getKafkaConfiguration().getBootstrapServers());
+//        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+//        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+//        return props;
+//    }
+//
+//    @Bean
+//    public ProducerFactory<String, String> producerFactory(@Autowired EnvironmentConfiguration environmentConfiguration) {
+//        return new DefaultKafkaProducerFactory<>(producerConfig(environmentConfiguration));
+//    }
+//
+//
+//    @Bean
+//    public KafkaTemplate<String, String> kafkaTemplate(ProducerFactory<String, String> producerFactory) {
+//        return new KafkaTemplate<>(producerFactory);
+//    }
 
 }
